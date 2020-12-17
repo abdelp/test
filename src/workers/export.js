5cc6dec8e701c870bd996678b08e0427c1ee8951
Abdel <juniorperezpy@gmail.com>
2020-12-17 12:07:52 -0300

    Add initial version of workers

--- src/workers/export.js
+++ src/workers/export.js
@@ -1,1 +1,20 @@
-// importScripts('https://unpkg.com/idb-keyval@latest/dist/idb-keyval-iife.min.js');
+importScripts('./idb-keyval-iife.min.js');
+
+self.onmessage = async ($event) => {
+  if ($event && $event.data && $event.data.msg === 'saveUser') {
+    const user = await saveUser();
+    self.postMessage(user);
+  }
+};
+
+async function saveUser(user) {
+  const keys = await idbKeyval.keys();
+
+  let sum = 0;
+  for (const key of keys) {
+      const value = await idbKeyval.get(key);
+      sum += value;
+  }
+
+  return sum;
+}
