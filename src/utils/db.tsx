b482d2562eab9812020f7f9da0c9ff3f08344246
Abdel <juniorperezpy@gmail.com>
2021-01-23 17:18:53 -0300

    Add obtenerIndiceDeUsuarioTesteadoPorCedula and actualizarDatosUsuarioTesteadoPorCedula

--- src/utils/db.tsx
+++ src/utils/db.tsx
@@ -1,4 +1,17 @@
 import { set, get } from 'idb-keyval';
+import _ from 'lodash';
+
+const obtenerIndiceDeUsuarioTesteadoPorCedula = async (cedula: any) => {
+  try {
+    const TABLA = 'usuarios_testeados';
+    const usuariosTesteados: any = await get(TABLA) || [];
+    const usuarioTesteadoIdx = usuariosTesteados.findIndex((u: any) => u.cedula === cedula);
+
+    return usuarioTesteadoIdx;
+  } catch(e) {
+    throw e;
+  }
+};
 
 const obtenerDatosUsuarioTesteadoPorCedula = async (cedula: any) => {
   try {
@@ -16,15 +29,55 @@ const obtenerDatosUsuarioTesteadoPorCedula = async (cedula: any) => {
 const actualizarDatosUsuarioTesteadoPorCedula = async (cedula: any, datos: any) => {
   try {
     const TABLA = 'usuarios_testeados';
-    const DATOS_USUARIO = obtenerDatosUsuarioTesteadoPorCedula(cedula) || {};
-    const NUEVOS_DATOS = {...DATOS_USUARIO, ...datos};
+    const DATOS_USUARIO = await obtenerDatosUsuarioTesteadoPorCedula(cedula) || {};
+    const NUEVOS_DATOS = _.merge(DATOS_USUARIO, datos);
 
-    await set(TABLA, NUEVOS_DATOS);
+    let usuariosTesteados: any = await get(TABLA) || [];
+    const USUARIO_TESTEADO_IDX = await obtenerIndiceDeUsuarioTesteadoPorCedula(cedula);
+
+    if (USUARIO_TESTEADO_IDX === -1) return {cod: 0, mensaje: `Usuario con cédula de identidad nro.: ${cedula} no encontrado`};
+
+    usuariosTesteados[USUARIO_TESTEADO_IDX] = NUEVOS_DATOS;
+
+    await set(TABLA, usuariosTesteados);
 
     return {cod: 0, mensaje: 'Los datos del usuario testeado fueron actualizados exitosamente'};
   } catch(e) {
     throw e;
   }
+};
+
+const actualizarTestDeUsuario = async (
+  cedula: any,
+  categoria: any,
+  test: any,
+  data: any) => {
+
+  const TABLA = 'usuarios_testeados';
+  const FECHA = new Date();
+  const USUARIOS_TESTEADOS: any = await get(TABLA) || [];
+  const usuarioTesteado: any = await obtenerDatosUsuarioTesteadoPorCedula(cedula) || {};
+
+  let nuevoExamen: any = {
+    [categoria]: {
+      [test]: data
+    }
+  };
+
+
+  // const idx = usuariosTesteados.findIndex((u: any) => u.ci === ci);
+  // let cat: any;
+
+  // if (!usuariosTesteados[idx][categoria.toLowerCase()]) {
+  //   cat = usuariosTesteados[idx]["examenes"][categoria.toLowerCase()] = {};
+  // } else {
+  //   cat = usuariosTesteados[idx]["examenes"][categoria.toLowerCase()];
+  // }
+
+  // cat[test] = { date: new Date(), result }
+  // await set("usuarios_testeados", usuariosTesteados);
+
+  // return;
 };
 
 const update = async (table: string, data: any) => {
