a171d75c2eb58d18dc0b71c46ae107dd93b5fb6a
Abdel <juniorperezpy@gmail.com>
2021-01-23 15:10:51 -0300

    Add obtenerDatosUsuarioPorCedula method

--- src/utils/db.tsx
+++ src/utils/db.tsx
@@ -1,13 +1,68 @@
-import userEvent from '@testing-library/user-event';
 import { set, get } from 'idb-keyval';
-import { useCookies } from "react-cookie";
 
-async function loggedIn() {
-  const user = await get("user");
-  return user;
-}
+const obtenerDatosUsuarioTesteadoPorCedula = async (cedula: any) => {
+  try {
+    const TABLA = 'usuarios_testeados';
+    const usuariosTesteados: any = await get(TABLA);
+    const usuarioTesteado = usuariosTesteados.findIndex((u: any) => u.cedula === cedula);
 
-async function updateUserTest(ci: any, categoria: any, test: any, result: any) {
+    return usuarioTesteado;
+  } catch(e) {
+    throw e;
+  }
+};
+
+const actualizarUsuarioTesteadoPorCedula = async (cedula: any, datos: any) => {
+  const TABLA = 'usuarios_testeados';
+  const DATOS_USUARIO = obtenerDatosUsuarioTesteadoPorCedula(cedula);
+
+  // VER QUE PASA CUANDO NO HAY DATOS
+
+  const NUEVOS_DATOS = {...DATOS_USUARIO, ...datos};
+
+
+};
+
+const update = async (table: string, data: any) => {
+  try {
+    await set(table, data);
+    return {code: 0, message: 'Table updated successfully'};
+  } catch (e) {
+    return e;
+  }
+};
+
+const actualizarFechaDeTest = async (cedula: any, categoria: string, test: string, fecha: any) => {
+  try {
+    const TABLA = 'usuarios_testeados';
+    const usuariosTesteados: any = await get(TABLA);
+
+    if (usuariosTesteados?.length) {
+      const usuarioIdx = usuariosTesteados.findIndex((u: any) => u.cedula === cedula);
+      const usuarioTesteado = usuariosTesteados[usuarioIdx];
+      const CATEGORIA = usuarioTesteado.categoria || {};
+      let testGuardado = usuarioTesteado.categoria[test] || {};
+
+      testGuardado.fecha = new Date();
+
+      console.log(usuarioTesteado);
+      console.log(usuariosTesteados);
+
+    } else {
+      
+      
+    }
+
+    await set(TABLA, usuariosTesteados);
+
+    return {codigo: 0, mensaje: 'Fecha de test actualizada correctamente'};
+
+  } catch(e) {
+    throw e;
+  }
+};
+
+const updateUserTest = async (ci: any, categoria: any, test: any, result: any) => {
   const date = new Date();
   const usuariosTesteados: any = await get('usuarios_testeados');
   const idx = usuariosTesteados.findIndex((u: any) => u.ci === ci);
@@ -25,4 +80,4 @@ async function updateUserTest(ci: any, categoria: any, test: any, result: any) {
   return;
 }
 
-export { loggedIn, updateUserTest }
+export { updateUserTest }
