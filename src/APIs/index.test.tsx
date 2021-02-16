a5044004dda1f32afa426771d484a5a751f9c83d
Abdel <juniorperezpy@gmail.com>
2021-02-16 15:09:23 -0300

    Add when there's no user found to be tested jest test

--- src/APIs/index.test.tsx
+++ src/APIs/index.test.tsx
@@ -8,7 +8,7 @@ const mockedAxios = axios as jest.Mocked<typeof axios>;
 jest.mock("@ionic-native/http");
 const mockedHTTP = HTTP as jest.Mocked<typeof HTTP>;
 
-describe("obtenerDatosUsuarioTesteado", () => { 
+describe("obtenerDatosUsuarioTesteado", () => {
   describe("obtiene exitosamente los datos del usuario a ser testeado", () => {
     const data = `<?xml version="1.0" encoding="utf-8"?>
       <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
@@ -33,8 +33,14 @@ describe("obtenerDatosUsuarioTesteado", () => {
           </ConsultarAntecedenteResponse>
         </soap:Body>
       </soap:Envelope>`;
-      
-    const PostResponse = { data, status: 200, statusText: "OK", headers: {}, url: '' };
+
+    const PostResponse = {
+      data,
+      status: 200,
+      statusText: "OK",
+      headers: {},
+      url: "",
+    };
 
     const expectedResult = {
       apellidos: "ANTUNEZ VILLAGRA ",
@@ -48,25 +54,79 @@ describe("obtenerDatosUsuarioTesteado", () => {
 
     it("con el plugin nativo HTTP", async () => {
       mockedHTTP.post.mockResolvedValueOnce(PostResponse);
-  
-      await expect(
-        obtenerDatosUsuarioTesteado("valid token", "0", "cedula")
-      ).resolves.toEqual(expectedResult);
-    });
-    
-    it("con axios", async () => {
-      mockedHTTP.post.mockRejectedValueOnce('cordova_not_available');
-      mockedAxios.post.mockResolvedValueOnce(PostResponse);
-  
-      await expect(
-        obtenerDatosUsuarioTesteado("valid token", "0", "cedula")
-      ).resolves.toEqual(expectedResult);
-    });
-  })
 
-  describe("obtiene erroneamente los datos del usuario a ser testeado", () => { 
-    it('con el plugin nativo HTTP', async () => {
-      const error = {"status": -1, "error": "There was an error with the request: Failed to connect to www.opaci.org.py/190.128.227.98:8082"};
+      await expect(
+        obtenerDatosUsuarioTesteado("valid token", "0", "cedula")
+      ).resolves.toEqual(expectedResult);
+    });
+
+    it("con axios", async () => {
+      mockedHTTP.post.mockRejectedValueOnce("cordova_not_available");
+      mockedAxios.post.mockResolvedValueOnce(PostResponse);
+
+      await expect(
+        obtenerDatosUsuarioTesteado("valid token", "0", "cedula")
+      ).resolves.toEqual(expectedResult);
+    });
+  });
+
+  describe("retorna antecedente vacío cuando no encuentra el usuario a ser testeado", () => {
+    const data = `<?xml version="1.0" encoding="utf-8"?>
+    <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+      <soap:Body>
+        <ConsultarAntecedenteResponse xmlns="http://rut.antsv.gov.py/">
+          <ConsultarAntecedenteResult>
+            <CodError>0</CodError>
+            <Antecedentes />
+            <Cantidad>0</Cantidad>
+          </ConsultarAntecedenteResult>
+        </ConsultarAntecedenteResponse>
+      </soap:Body>
+    </soap:Envelope>`;
+
+    const PostResponse = {
+      data,
+      status: 200,
+      statusText: "OK",
+      headers: {},
+      url: "",
+    };
+
+    const expectedResult = {
+      apellidos: null,
+      cantidad: "0",
+      categoria: null,
+      idAntecedente: null,
+      nombres: null,
+      nroDocumento: "0",
+      tramite: null,
+    };
+
+    it("con el plugin nativo HTTP", async () => {
+      mockedHTTP.post.mockResolvedValueOnce(PostResponse);
+
+      await expect(
+        obtenerDatosUsuarioTesteado("valid token", "0", "cedula")
+      ).resolves.toEqual(expectedResult);
+    });
+
+    it("con axios", async () => {
+      mockedHTTP.post.mockRejectedValueOnce("cordova_not_available");
+      mockedAxios.post.mockResolvedValueOnce(PostResponse);
+
+      await expect(
+        obtenerDatosUsuarioTesteado("valid token", "0", "cedula")
+      ).resolves.toEqual(expectedResult);
+    });
+  });
+
+  describe("obtiene erroneamente los datos del usuario a ser testeado", () => {
+    it("con el plugin nativo HTTP", async () => {
+      const error = {
+        status: -1,
+        error:
+          "There was an error with the request: Failed to connect to www.opaci.org.py/190.128.227.98:8082",
+      };
 
       mockedHTTP.post.mockRejectedValueOnce(error);
 
@@ -75,9 +135,9 @@ describe("obtenerDatosUsuarioTesteado", () => {
       ).rejects.toEqual(error);
     });
 
-    it('con axios', async () => {
+    it("con axios", async () => {
       const errorMessage = "Network Error";
-      mockedHTTP.post.mockRejectedValueOnce('cordova_not_available');
+      mockedHTTP.post.mockRejectedValueOnce("cordova_not_available");
 
       mockedAxios.post.mockImplementationOnce(() =>
         Promise.reject(new Error(errorMessage))
